15f8554cc75a08e3b47c271c51d1e268b63848b1
anhkt <anhkt@rikkeisoft.com>
2019-05-30 10:02:45 +0700

    custom underline

---
+++ app/src/main/java/com/example/kotlinapplication/DemoSplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinapplication
+
+import android.R
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.widget.TextView
+
+
+class DemoSplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_demo_splash)
+//        val tv: TextView = findViewById(R.id.tv)
+//
+//        val content = SpannableString("ahihihi con mem me")
+//        content.setSpan(UnderlineSpan(), 0, content.length, 0)
+//        tv.text = content
+    }
+}
