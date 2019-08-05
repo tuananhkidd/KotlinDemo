abba3063b8ca285404c9d6416b916eaf46a73f07
Tuan Anh Kidd <tuaananh.khong.96@gmail.com>
2019-08-05 15:59:12 +0700

    add cm 3

--- app/src/main/java/com/example/kotlinapplication/Test.kt
+++ app/src/main/java/com/example/kotlinapplication/Test.kt
@@ -2,7 +2,7 @@ package com.example.kotlinapplication
 
 class Test {
     private var name: String? = "aaaaa"
-    private var ahihi1:String? = "rebase"
+    private var ahaha:String? = "rebase"
 
     fun getName(): String {
         return name!!
