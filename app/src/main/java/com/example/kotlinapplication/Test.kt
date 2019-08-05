7514cf8a538e6bc639c3579a876d9362f9e665c3
Tuan Anh Kidd <tuaananh.khong.96@gmail.com>
2019-08-05 17:06:12 +0700

    dm rb1

--- app/src/main/java/com/example/kotlinapplication/Test.kt
+++ app/src/main/java/com/example/kotlinapplication/Test.kt
@@ -1,7 +1,7 @@
 package com.example.kotlinapplication
 
 class Test {
-    private var name: String? = "234354 2321"
+    private var name: String? = "234354 2321 123"
     private var ahaha:String? = "rebase nao ae 23 21312"
 
     fun getName(): String {
