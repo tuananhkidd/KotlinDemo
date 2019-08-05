3b87a0bae61261f7c8f6d9b37bc81a0a26321690
Tuan Anh Kidd <tuaananh.khong.96@gmail.com>
2019-08-05 15:52:55 +0700

    test rebase 2

--- app/src/main/java/com/example/kotlinapplication/Test.kt
+++ app/src/main/java/com/example/kotlinapplication/Test.kt
@@ -1,7 +1,7 @@
 package com.example.kotlinapplication
 
 class Test {
-    private var name: String? = ""
+    private var name: String? = "huhu"
     private var ahuhu:String? = "123"
 
     fun getName(): String {
