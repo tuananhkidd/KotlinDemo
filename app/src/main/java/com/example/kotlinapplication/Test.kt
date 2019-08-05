52ae55081bc49aa4f2014618b8c2178857289b4a
Tuan Anh Kidd <tuaananh.khong.96@gmail.com>
2019-08-05 16:12:10 +0700

    rb demo

--- app/src/main/java/com/example/kotlinapplication/Test.kt
+++ app/src/main/java/com/example/kotlinapplication/Test.kt
@@ -1,8 +1,8 @@
 package com.example.kotlinapplication
 
 class Test {
-    private var name: String? = "huhu"
-    private var ahuhu:String? = "123"
+    private var name: String? = "huhu 12 3"
+    private var ahuhu:String? = "123 434 "
 
     fun getName(): String {
         return name!!
